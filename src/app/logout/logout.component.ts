5d8d28d204573bb163796704fdf166fb7b1570b5
prahllad roy <prahllad.x@venturepact.com>
2017-04-24 14:33:24 +0530

    new updates

---
+++ src/app/logout/logout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'logout',
+    templateUrl: 'logout.component.html',
+    styleUrls: ['logout.component.css']
+})
+export class LogoutComponent {
+
+}
